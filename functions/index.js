754bccbaa8ba85ed140630fd9fc39fc14dba9356
Khalid, Umair <Umair.Khalid@McKesson.com>
2019-04-13 23:47:28 -0700

    triggers on cloud firestore events

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,23 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// Cloud Firestore Triggers
+exports.staffAdded = functions.firestore.document('staff/{staffId}').onCreate((snap, context) => {
+    console.info('A new staff member was added');
+});
+
+exports.staffUpdated = functions.firestore.document('staff/{staffId}').onUpdate((snap, context) => {
+    console.info('A staff member was updated');
+});
+
+exports.staffDeleted = functions.firestore.document('staff/{staffId}').onDelete((snap, context) => {
+    console.log('A staff member was deleted');
+});
+
+exports.staffChanged = functions.firestore.document('staff/{staffId}').onWrite((snap, context) => {
+    console.log('A staff member was deleted');
+});
+
 exports.addPerson = functions.https.onCall((data, context) => {
     var firstName = data.firstName;
     var lastName = data.lastName;
