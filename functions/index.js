ad948cce3549df143d9387aca9e073022587d268
Khalid, Umair <Umair.Khalid@McKesson.com>
2019-04-13 22:37:07 -0700

    created functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.addPerson = functions.https.onCall((data, context) => {
+    var firstName = data.firstName;
+    var lastName = data.lastName;
+
+    return admin.database().ref('/persons').push({
+        firstName: firstName,
+        lastName: lastName,
+        fullName: firstName + " " + lastName
+    }).then(() => {
+        console.log('New person added');
+        return "Ok";
+    }).catch((error) => {
+        throw new functions.https.HttpsError('unknown', error.message, error)
+    })
+});
+
+
+exports.date = functions.https.onRequest((req, res) => {
+    if (req.method != 'GET') {
+        return res.status(403).send('Forbidden!');
+    }
+    const date = new Date();
+    const snapshot = admin.database().ref('/dates').push({now: date.toDateString()});
+    res.redirect(303, snapshot.ref.toString());
+    //res.send(date.toDateString());
 });
